515d611f5dd077f1e55e20726eb6fd7e497204a0
lukaopc <lucaspedrosocarvalho@gmail.com>
2016-03-12 18:08:15 -0300

    Print dos aquivos de entrada, saída e da palavra

--- src/main.java
+++ src/main.java
@@ -3,7 +3,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Teste");
+		System.out.print("Arquivo de Leitura: ");
+		System.out.println(args[0]);
+		System.out.print("Palavra de Entrada: ");
+		System.out.println(args[1]);
+		System.out.print("Arquivo de Saída: ");
+		System.out.println(args[2]);
+
 	
 	}
 	
