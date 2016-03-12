c0637b255a9d678a17f4264cf836dd16b85855fe
lukaopc <lucaspedrosocarvalho@gmail.com>
2016-03-12 18:28:42 -0300

    Leitura do arquivo

--- src/main.java
+++ src/main.java
@@ -1,15 +1,32 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		String arqentrada = args[0];
+		String palavra = args[1];
+		String arqsaida = args[2];
+
 		
 		System.out.print("Arquivo de Leitura: ");
-		System.out.println(args[0]);
-		System.out.print("Palavra de Entrada: ");
-		System.out.println(args[1]);
+		System.out.println(arqentrada);
 		System.out.print("Arquivo de Saída: ");
-		System.out.println(args[2]);
-
+		System.out.println(arqsaida);
+		
+		System.out.print("Palavra de Entrada: ");
+		System.out.println(palavra);
+		
+		
+		Scanner scanner = new Scanner(new FileReader(arqentrada)).useDelimiter("\\||\\n");
+		// O useDelimiter("\\||\\n") especifica como que será pulado durante a leitura
+		while (scanner.hasNext()) {
+			String teste = scanner.next();
+			System.out.println(teste); // Teste
+		}
+		
 	
 	}
 	
