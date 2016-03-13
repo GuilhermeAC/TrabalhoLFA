c35654399376e1e3473771ef8796836a43c5ab1d
lukaopc <lucaspedrosocarvalho@gmail.com>
2016-03-12 23:05:13 -0300

    Leitura do arquivo, salvando os dados na Hash com List 2

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,6 @@ import java.util.Map;
 
 public class Main {
 
-<<<<<<< HEAD:src/main.java
 	public static void main(String[] args) throws FileNotFoundException {
 		
 		String arqentrada = args[0];
@@ -72,16 +71,6 @@ public class Main {
             System.out.println(values);
         }
 		
-=======
-	public static void main(String[] args) {
-		
-		System.out.print("Arquivo de Leitura: ");
-		System.out.println(args[0]);
-		System.out.print("Palavra de Entrada: ");
-		System.out.println(args[1]);
-		System.out.print("Arquivo de Saida: ");
-		System.out.println(args[2]);
->>>>>>> origin/master:src/Main.java
 
 	}
 
