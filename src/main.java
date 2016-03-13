53305777202fdf03d859b07e24252a0b014a01f5
lukaopc <lucaspedrosocarvalho@gmail.com>
2016-03-12 22:59:45 -0300

    Leitura do arquivo, salvando os dados na Hash com List

--- src/main.java
+++ src/main.java
@@ -1,6 +1,10 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
@@ -11,23 +15,63 @@ public class Main {
 		String arqsaida = args[2];
 
 		
-		System.out.print("Arquivo de Leitura: ");
-		System.out.println(arqentrada);
-		System.out.print("Arquivo de Saída: ");
-		System.out.println(arqsaida);
+		//System.out.print("Arquivo de Leitura: ");
+		//System.out.println(arqentrada);
+		//System.out.print("Arquivo de Saída: ");
+		//System.out.println(arqsaida);
 		
-		System.out.print("Palavra de Entrada: ");
-		System.out.println(palavra);
+		//System.out.print("Palavra de Entrada: ");
+		//System.out.println(palavra);
 		
+		Map<String, List<String>> regrasGLC = new HashMap<String, List<String>>();
 		
-		Scanner scanner = new Scanner(new FileReader(arqentrada)).useDelimiter("\\||\\n");
-		// O useDelimiter("\\||\\n") especifica como que será pulado durante a leitura
-		while (scanner.hasNext()) {
-			String teste = scanner.next();
-			System.out.println(teste); // Teste
+		String esquerda = null;
+		String direita;
+		String simbolo;
+				
+		// O useDelimiter("[^aA-zZ]+") especifica como que será pulado durante a leitura
+		Scanner leitura = new Scanner(new FileReader(arqentrada)).useDelimiter("[^aA-zZ]+");
+		
+		while (leitura.hasNext()) {
+			
+			simbolo = leitura.next();
+			
+			if (simbolo.matches("[A-Z]")) {
+				
+				esquerda = simbolo;
+				
+			}
+			
+			else {
+				
+				direita = simbolo;
+				
+				if (regrasGLC.get(direita) == null) {
+				
+					List<String> lista = new ArrayList<String>();	
+					regrasGLC.put(direita, lista);
+					
+				}
+				
+				List<String> listaAux = regrasGLC.get(direita);
+				listaAux.add(esquerda);
+				regrasGLC.put(direita, listaAux);
+								
+			}
+				
 		}
+				
+		leitura.close();	
 		
-	
+		
+        for (Map.Entry<String, List<String>> entry : regrasGLC.entrySet()) {
+            String key = entry.getKey();
+            List<String> values = entry.getValue();
+            System.out.print(key + " = ");
+            System.out.println(values);
+        }
+		
+
 	}
-	
+
 }
